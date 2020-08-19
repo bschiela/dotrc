9c14674520bf6be18eba8d856af1da837792d7ba
wfsch <bschiela@gmail.com>
2020-08-18 22:41:08 -0400

    Configure emacs with use-package and Evil mode
    init.el is the analog of .vimrc. This initial configuration was cobbled
    together from the following sources:
    
    [1] https://blog.aaronbieber.com/2015/05/24/from-vim-to-emacs-in-fourteen-days.html
    [2] http://cachestocaches.com/2015/8/getting-started-use-package/
    [3] https://github.com/jwiegley/use-package
    [4] https://github.com/emacs-evil/evil
    [5] https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html#Saving-Customizations
    
    It addresses the following items:
    - MELPA is a popular Emacs Lisp Package Archive (ELPA) that extends the
      default GNU ELPA archive, analogous to conda-forge.
    - `use-package` handles auto-installation and deferred loading of
      packages.
    - `auto-package-update` (self explanatory).
    - Evil mode is a vim emulator for emacs that provides vim-like
      keybindings and modal editing functionality.
    - Auto-generated customization code is written to `custom-file` to keep
      init.el and version control clean.

---
+++ emacs.d/init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/")
+)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package)
+)
+(eval-when-compile (require 'use-package))
+
+(use-package auto-package-update
+             :ensure t
+             :config
+             (setq auto-package-update-delete-old-versions t)
+             (setq auto-package-update-hide-results t)
+             (auto-package-update-maybe))
+(use-package evil :ensure t :config (evil-mode t))
+
+(setq custom-file "~/.emacs.d/.emacs-custom.el")
+(load custom-file)
