468bbe58c140e0cbf84e3a5abc5a87471288da7f
wfsch <bschiela@gmail.com>
2020-08-25 21:48:55 -0400

    Initial configuration of Evil and Org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,19 +9,35 @@
 ;;; use-package
 (unless (package-installed-p 'use-package)
         (package-refresh-contents)
-        (package-install 'use-package)
-)
+        (package-install 'use-package))
 (eval-when-compile (require 'use-package))
 
 ;;; packages
 (use-package auto-package-update
-             :ensure t
-             :config
-             (setq auto-package-update-delete-old-versions t)
-             (setq auto-package-update-hide-results t)
-             (auto-package-update-maybe))
-(use-package evil :ensure t :config (evil-mode t))
-(use-package org :ensure t)
+    :ensure t
+    :config
+    (setq auto-package-update-delete-old-versions t)
+    (setq auto-package-update-hide-results t)
+    (auto-package-update-maybe))
+(use-package evil
+    :ensure t
+    :init (setq evil-want-C-u-scroll t)
+    :config (evil-mode t))
+(use-package org
+    :ensure t
+    :config
+    (global-set-key (kbd "C-c a") 'org-agenda)
+    (global-set-key (kbd "C-c c") 'org-capture)
+    (global-set-key (kbd "C-c l") 'org-store-link)
+    (setq org-todo-keywords
+        '((sequence "TODO" "NEXT" "BLOCKED" "BACKLOG" "CANCELLED" "DONE")))
+    (setq org-log-done t)
+    ;;; clocking
+    (org-clock-persistence-insinuate)
+    (setq org-clock-persist 'clock)
+    (setq org-clock-into-drawer t)
+    (setq org-clock-out-when-done t)
+    (setq org-log-note-clock-out t))
 
 ;;; configuration
 
