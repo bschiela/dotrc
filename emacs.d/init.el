56cbd19f9372df7b40705df3d07c2e77b6c30560
wfsch <bschiela@gmail.com>
2020-09-06 23:44:41 -0400

    Trial run evil-org keybindings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -74,3 +74,11 @@
     :ensure t
     :config
     (powerline-center-evil-theme))
+(use-package evil-org
+    :ensure t
+    :after org
+    :config
+    (add-hook 'org-mode-hook 'evil-org-mode)
+    (add-hook 'evil-org-mode-hook (lambda () (evil-org-set-key-theme)))
+    (require 'evil-org-agenda)
+    (evil-org-agenda-set-keys))
