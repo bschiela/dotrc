d1b64352c5e5d39bafa22a8a22f1d24b9ade2357
wfsch <bschiela@gmail.com>
2020-09-16 21:13:52 -0400

    Remove auto-package-update
    An update (maybe Org?) broke evil-org-mode's binding of the agenda view
    dispatch function to the `z` key. Now suddenly it's bound to `gD`.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,12 +20,6 @@
 (eval-when-compile (require 'use-package))
 
 ;;; packages
-(use-package auto-package-update
-    :ensure t
-    :config
-    (setq auto-package-update-delete-old-versions t)
-    (setq auto-package-update-hide-results t)
-    (auto-package-update-maybe))
 (use-package evil
     :ensure t
     :config (evil-mode t))
