fc3bf5270fcb17d2fb4be8e68d703f1b19090f7c
wfsch <bschiela@gmail.com>
2020-12-27 20:31:37 -0500

    Get hyperlinks to open in browser from WSL
    https://adam.kruszewski.name/2017/09/emacs-in-wsl-and-opening-links/

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,14 @@
 
 (setq-default fill-column 80)
 
+;;; open links in browser from WSL
+(let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
+      (cmd-args '("/c" "start")))
+    (when (file-exists-p cmd-exe)
+      (setq browse-url-generic-program  cmd-exe
+            browse-url-generic-args     cmd-args
+            browse-url-browser-function 'browse-url-generic)))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives
