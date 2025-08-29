489ada76e626c56e61e7f1cbe46bdd0d04ade137
William F Schiela <bschiela@gmail.com>
2025-08-29 14:35:01 -0400

    Add latitude and longitude of NYC
    This allows sunrise/sunset times to be shown in org agenda with
    `S`/`gs` (emacs/evil).

--- emacs.d/.emacs-custom.el
+++ emacs.d/.emacs-custom.el
@@ -4,6 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(calendar-latitude 40.7128)
+ '(calendar-location-name "New York City")
+ '(calendar-longitude -74.006)
  '(org-agenda-files
    '("~/workspace/orgmode/main.org" "~/workspace/orgmode/log.org"))
  '(package-selected-packages nil))
